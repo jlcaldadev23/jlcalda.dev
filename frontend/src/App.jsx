3cb1bb3a137d75f054d54352a035e716d9764e6f
JLCalda <jlcaldadev23@gmail.com>
2022-12-03 10:07:03 +0800

    initial commit , boilerplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App text-3xl bg-slate-400">
+      <img src="./assets/logo.png" alt="logo" />
+      APP
+    </div>
+  );
+}
+
+export default App;
